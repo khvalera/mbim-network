ce8fed6a8ea16a906df4cc63ce95ee65d7195d45
khvalera <khvalera@ukr.net>
2023-11-21 23:36:10 +0200

    v1.0.0

---
+++ language/en_US/network_mbim_lang.php
@@ -0,0 +1,29 @@
+<?php
+$lang['network_mbim_app_name'] = 'MBIM  Network';
+$lang['network_mbim_app_description'] = 'The MBIM Network application provides the ability to configure and manage modems that support the MBIM protocol.';
+$lang['network_mbim_settings'] = 'Network MBIM Settings';
+$lang['network_mbim_apn'] = 'APN';
+$lang['network_mbim_apn_user'] = 'APN User';
+$lang['network_mbim_apn_pass'] = 'APN Password';
+$lang['network_mbim_proxy'] = 'Proxy';
+$lang['network_mbim_device'] = 'Device';
+$lang['network_mbim_modem_status'] = 'Modem status';
+$lang['network_mbim_data_from_modem'] = 'Data from modem';
+$lang['network_mbim_get_data_modem'] = 'Get data from modem';
+$lang['network_mbim_query_device_caps'] = 'Query device capabilities';
+$lang['network_mbim_query_subscriber_ready_status'] = 'Query subscriber ready status';
+$lang['network_mbim_query_radio_state'] = 'Query radio state';
+$lang['network_mbim_query_device_services'] = 'Query device services';
+$lang['network_mbim_query_pin_state'] = 'Query PIN state';
+$lang['network_mbim_query_home_provider'] = 'Query home provider';
+$lang['network_mbim_query_preferred_providers'] = 'Query preferred providers';
+$lang['network_mbim_query_registration_state'] = 'Query registration state';
+$lang['network_mbim_query_signal_state'] = 'Query signal state';
+$lang['network_mbim_query_packet_service_state'] = 'Query packet service state';
+$lang['network_mbim_query_connection_state'] = 'Query connection state';
+$lang['network_mbim_query_ip_configuration'] = 'Query IP configuration';
+$lang['network_mbim_query_packet_statistics'] = 'Query packet statistics';
+$lang['network_mbim_phonebook_read_all'] = 'Read all phonebook entries';
+$lang['network_mbim_error_data_modem'] = 'Error receiving data from modem';
+
+
---
+++ language/uk/network_mbim_lang.php
@@ -0,0 +1,29 @@
+<?php
+$lang['network_mbim_app_name'] = 'MBIM  Network';
+$lang['network_mbim_app_description'] = 'Додаток MBIM Network надає можливість налаштовувати та керувати модемами, які підтримують протокол MBIM.';
+$lang['network_mbim_settings'] = 'Налаштування мережі MBIM';
+$lang['network_mbim_apn'] = 'APN';
+$lang['network_mbim_apn_user'] = 'APN користувач';
+$lang['network_mbim_apn_pass'] = 'APN пароль';
+$lang['network_mbim_proxy'] = 'Проксі';
+$lang['network_mbim_device'] = 'Пристрій';
+$lang['network_mbim_modem_status'] = 'Стан модему';
+$lang['network_mbim_data_from_modem'] = 'Дані з модему';
+$lang['network_mbim_get_data_modem'] = 'Отримати дані з модему';
+$lang['network_mbim_query_device_caps'] = 'Запит можливостей пристрою';
+$lang['network_mbim_query_subscriber_ready_status'] = 'Запит статусу готовності абонента';
+$lang['network_mbim_query_radio_state'] = 'Запит стану радіо';
+$lang['network_mbim_query_device_services'] = 'Запит послуг пристрою';
+$lang['network_mbim_query_pin_state'] = 'Запит стану PIN-коду';
+$lang['network_mbim_query_home_provider'] = 'Запит домашнього провайдера';
+$lang['network_mbim_query_registration_state'] = 'Запит стану реєстрації';
+$lang['network_mbim_query_preferred_providers'] = 'Запит бажаних постачальників';
+$lang['network_mbim_query_signal_state'] = 'Запит стану сигналу';
+$lang['network_mbim_query_packet_service_state'] = 'Запит стану служби пакетів';
+$lang['network_mbim_query_connection_state'] = 'Запит стану підключення';
+$lang['network_mbim_query_ip_configuration'] = 'Запит конфігурації IP';
+$lang['network_mbim_query_packet_statistics'] = 'Запит статистикі пакетів';
+$lang['network_mbim_phonebook_read_all'] = 'Читання всіх записів телефонної книги';
+$lang['network_mbim_error_data_modem'] = 'Помилка отримання даних від модему';
+
+
